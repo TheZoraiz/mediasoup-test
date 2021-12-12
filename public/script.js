6226acde52ee1c67a0e3d4f67b489cf98903d083
Zoraiz Hassan <hzoraiz8@gmail.com>
2021-12-12 17:38:07 +0500

    Completed 1 way receive

--- public/script.js
+++ public/script.js
@@ -7,15 +7,19 @@ const roomField = document.getElementById("roomNum")
 const inputField = document.getElementById("roomInfo")
 const videoFeed = document.getElementById("videos")
 
-const toggelButtons = document.getElementById("toggle-buttons")
-const toggelVideo = document.getElementById("toggle-video")
-const toggelAudio = document.getElementById("toggle-audio")
+const toggleButtons = document.getElementById("toggle-buttons")
+const toggleVideo = document.getElementById("toggle-video")
+const toggleAudio = document.getElementById("toggle-audio")
 
 const socket = io('http://localhost:3000')
 
 let roomName
 let device
+let routerRtpCapabilities
 let producerTransport
+let localProducer
+let isCreator
+let consumers = []
 
 socket.on('connected', ({ socketId }) => {
     console.log(socketId)
@@ -31,137 +35,251 @@ const mediaConstraints = {
     }
 }
 
-let params = {
+let globalParams = {
     // mediasoup params
+    encoding: [
+        {
+            rid: 'r0',
+            maxBitrate: 100000,
+            scalabilityMode: 'S1T3',
+        },
+        {
+            rid: 'r1',
+            maxBitrate: 300000,
+            scalabilityMode: 'S1T3',
+        },
+        {
+            rid: 'r2',
+            maxBitrate: 900000,
+            scalabilityMode: 'S1T3',
+        },
+    ],
+    codecOptions: {
+        videoGoogleStartBitrate: 1000,
+    }
 }
 
 submitButton.onclick = async() => {
     message.style = "display: none"
     inputField.style = "display: none"
     videoFeed.style = "display: flex"
-    toggelButtons.style = "display: block"
+    toggleButtons.style = "display: block"
 
-    let stream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
-    localStream = stream
-    localVideo.srcObject = stream
-    localVideo.muted = true
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
+        localVideo.srcObject = stream
+        localVideo.muted = true
+    
+        let track = stream.getVideoTracks()[0]
+    
+        globalParams = {
+            track,
+            ...globalParams,
+        }
+       console.log('globalParams', globalParams)
 
-    stream.getTracks().forEach(track => {
-        params[track.kind] = track
-        // if(track.kind == 'video') {
-        //     params = {
-        //         video: track,
-        //         ...params
-        //     }
-        // } else {
-        //     params = {
-        //         audio: track,
-        //         ...params
-        //     }
-        // }
-    })
+       roomName = roomField.value
+       socket.emit("create-or-join", roomName)
 
-    roomName = roomField.value
-    socket.emit("create-or-join", roomName)
+    } catch(error) {
+        console.error('Couldn\'t get local media stream', error)
+    }   
 }
 
-const createDevice = async(rtpCapabilities) => {
+socket.on('rtp-capabilities', async(data) => {
+    routerRtpCapabilities = data.rtpCapabilities
+    isCreator = data.isCreator
+    await createDevice()
+})
+
+const createDevice = async() => {
     device = new mediasoupClient.Device()
-    await device.load({ routerRtpCapabilities: rtpCapabilities })
+    await device.load({ routerRtpCapabilities: routerRtpCapabilities })
 
-    console.log('Device created:', device)
+    console.log('Device created', device)
 
-    socket.emit('web-rtc-transport', { sender: true, room: roomName }, ({ params }) => {
+    socket.emit('web-rtc-transport', { sender: isCreator, room: roomName }, async({ params }) => {
         // Callback fires when we get transport parameters on the server-side after it's created
         if(params.error) {
             console.log(params.error)
             return
         }
 
-        console.log('Transport params recieved:', params)
+        console.log('Router transport params recieved', params)
 
-        // Create local send transport
-        producerTransport = device.createSendTransport(params)
+        console.log('isCreator', isCreator)
+        if(isCreator) {
+            // Create local send transport
+            producerTransport = await device.createSendTransport(params)
+            assignProducerTransportEvents()
+            console.log('producerTransport', producerTransport)
 
-        producerTransport.on('connect', async({ dtlsParameters }, callback, errback) => {
-            try {
-                // Send local DTLS transport paramters to server-side trasnport
-                await socket.emit('transport-connect', {
-                    transportId: producerTransport.id,
-                    dtlsParameters
+            localProducer = await producerTransport.produce(globalParams)
+            assignProducerEvents()
+            console.log('localProducer', localProducer)
+
+        } else {
+            let tempConsumer = {}
+            // Create receive send transport
+            tempConsumer['consumerTransport'] = await device.createRecvTransport(params)
+            assignConsumerTransportEvents(tempConsumer.consumerTransport)
+            console.log('consumerTransport', tempConsumer.consumerTransport)
+
+            await socket.emit('consume', { rtpCapabilities: device.rtpCapabilities, room: roomName }, async({ params }) => {
+                if(params && params.error) {
+                    console.log(params.error)
+                    return
+                }
+        
+                console.log('Received params after \'consume\'', params)
+
+                tempConsumer['consumer'] = await tempConsumer.consumerTransport.consume({
+                    id: params.id,
+                    producerId: params.producerId,
+                    kind: params.kind,
+                    rtpParameters: params.rtpParameters
                 })
+                consumers.push(tempConsumer)
+                console.log('consumers array', consumers)
 
-                // Tell the local transport that paramters were submitted to server-side
-                callback()
-            } catch (error) {
-                errback(error)
-            }
-        })
+                const { track } = tempConsumer.consumer;
 
-        producerTransport.on('produce', async(parameters, callback, errback) => {
-            console.log(parameters)
+                console.log('Recieved track', track)
+    
+                let remoteVid = document.createElement('video')
+                remoteVid.srcObject = new MediaStream([track]);
+                remoteVid.id = params.producerId
+                remoteVid.autoplay = true
 
-            await socket.emit('transport-produce', {
-                transportId: producerTransport.id,
-                kind: parameters.kind,
-                rtpParameters: parameters.rtpParameters,
-                appData: parameters.appData
+                videoFeed.appendChild(remoteVid)
 
-            }, ({ id }) => {
-                // On socket.io callback tell local transport that parameters were submitted to server-side
-                // and provide it with server-side's transport's id
-                callback({ id })
+                socket.emit('consumer-resume', { room: roomName })
             })
-            try {
-                
-            } catch (error) {
-                errback(error)
-            }
-        })
+        }
+
     })
 }
 
-socket.on('rtp-capabilities', async(data) => {
-    let rtpCapabilities = data.rtpCapabilities
-    await createDevice(rtpCapabilities)
-})
+
+const assignProducerTransportEvents = () => {
+    producerTransport.on('connect', async({ dtlsParameters }, callback, errback) => {
+        try {
+            // Send local DTLS transport paramters to server-side transport
+            await socket.emit('transport-connect', {
+                // transportId: producerTransport.id,
+                dtlsParameters,
+                room: roomName,
+                isCreator
+            })
+
+            // Tell the local transport that paramters were submitted to server-side
+            callback()
+        } catch (error) {
+            errback(error)
+        }
+    })
+
+    producerTransport.on('produce', async(parameters, callback, errback) => {
+        console.log('Local \'producerTransport\' parameters', parameters)
+
+        try {
+            await socket.emit('transport-produce', {
+                // transportId: producerTransport.id,
+                kind: parameters.kind,
+                rtpParameters: parameters.rtpParameters,
+                appData: parameters.appData,
+                room: roomName
+
+            }, ({ id }) => {
+                // On socket.io callback, tell local transport that parameters were submitted to server-side
+                // and provide it with server-side's transport's id
+                callback({ id })
+            })
+        } catch (error) {
+            errback(error)
+        }
+    })
+}
+
+const assignProducerEvents = () => {
+    localProducer.on('trackended', () => {
+        // Close video track
+        console.log('Track ended...')
+    })
+
+    localProducer.on('transportclose', () => {
+        // Close video track
+        console.log('Transport closed...')
+    })
+}
+
+const assignConsumerTransportEvents = (consumerTransport) => {
+    
+    consumerTransport.on('connect', async({ dtlsParameters }, callback, errback) => {
+        try {
+            // Send local DTLS transport paramters to server-side transport
+            await socket.emit('transport-connect', {
+                dtlsParameters,
+                room: roomName,
+                isCreator
+            })
+
+            // Tell the local transport that paramters were submitted to server-side
+            callback()
+        } catch (error) {
+            errback(error)
+        }
+    })
+}
+
+const assignConsumerEvents = (consumer) => {
+    localProducer.on('trackended', () => {
+        // Close video track
+        console.log('Track ended...')
+    })
+
+    localProducer.on('transportclose', () => {
+        // Close video track
+        console.log('Transport closed...')
+    })
+}
 
 // Video and Audio button toggles
 
-toggelVideo.onclick = () => {
+toggleVideo.onclick = () => {
     let localStreamVidTracks = localStream.getVideoTracks()
     
-    if(toggelVideo.innerHTML == "Video On") {
+    if(toggleVideo.innerHTML == "Video On") {
         localStreamVidTracks.forEach(track => {
             track.enabled = false
         })
-        toggelVideo.innerHTML = "Video Off"
-        toggelVideo.style.backgroundColor = "rgb(255, 0, 0)"
+        toggleVideo.innerHTML = "Video Off"
+        toggleVideo.style.backgroundColor = "rgb(255, 0, 0)"
 
     } else {
         localStreamVidTracks.forEach(track => {
             track.enabled = true
         })
-        toggelVideo.innerHTML = "Video On"
-        toggelVideo.style.backgroundColor = "rgb(0, 255, 0)"
+        toggleVideo.innerHTML = "Video On"
+        toggleVideo.style.backgroundColor = "rgb(0, 255, 0)"
     }
 }
 
-toggelAudio.onclick = () => {
+toggleAudio.onclick = () => {
     let localStreamAudioTracks = localStream.getAudioTracks()
     
-    if(toggelAudio.innerHTML == "Audio On") {
+    if(toggleAudio.innerHTML == "Audio On") {
         localStreamAudioTracks.forEach(track => {
             track.enabled = false
         })
-        toggelAudio.innerHTML = "Audio Off"
-        toggelAudio.style.backgroundColor = "rgb(255, 0, 0)"
+        toggleAudio.innerHTML = "Audio Off"
+        toggleAudio.style.backgroundColor = "rgb(255, 0, 0)"
 
     } else {
         localStreamAudioTracks.forEach(track => {
             track.enabled = true
         })
-        toggelAudio.innerHTML = "Audio On"
-        toggelAudio.style.backgroundColor = "rgb(0, 255, 0)"
+        toggleAudio.innerHTML = "Audio On"
+        toggleAudio.style.backgroundColor = "rgb(0, 255, 0)"
     }
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express(cors())
 let http = require('http').Server(app)
 let io = require('socket.io')(http, {
     cors: {
-      origin: '*',
+        origin: '*',
     }
 })
 
@@ -15,7 +15,7 @@ const mediasoup = require('mediasoup')
 
 let worker
 
-const createWorker = async() => {
+const createWorker = async () => {
     worker = await mediasoup.createWorker({
         rtcMinPort: 10000,
         rtcMaxPort: 59999,
@@ -38,6 +38,7 @@ http.listen(port, () => {
 
 let producerTransport, consumerTransport
 let routers = {}
+let transports = {}
 let mediaCodecs = [
     {
         kind: 'audio',
@@ -52,33 +53,146 @@ let mediaCodecs = [
         parameters: {
             'x-google-start-bitrate': 1000
         }
-    }
+    },
+    {
+        kind: "video",
+        mimeType: "video/H264",
+        clockRate: 90000,
+        parameters: {
+          'packetization-mode': 1,
+          'level-asymmetry-allowed': 1,
+          'profile-level-id': "42e01f",
+        }
+    }    
 ]
 
-io.on('connection', async(socket) => {
-    
-    socket.on('create-or-join', async(room) => {
+io.on('connection', async (socket) => {
 
-        if (routers[room] == undefined) {
+    socket.on('create-or-join', async (room) => {
+
+        let isCreator
+
+        // if room exists
+        if (routers[room]) {
+            isCreator = false
+        } else {
             let newRouter = await worker.createRouter({ mediaCodecs })
             routers[room] = newRouter
 
-            console.log(routers)
-        } else {
-            
+            isCreator = true
+
+            console.log('New router added', routers)
         }
 
         io.to(socket.id).emit('rtp-capabilities', {
-            rtpCapabilities: routers[room].rtpCapabilities
+            rtpCapabilities: routers[room].rtpCapabilities,
+            isCreator
         })
     })
 
-    socket.on('web-rtc-transport', async({ sender, room }, callback) => {
-        if(sender) {
-            producerTransport = await createWebRtcTransport(room, callback)
+    socket.on('web-rtc-transport', async ({ sender, room }, callback) => {
+        if (sender) {
+            transports[room] = {
+                ...transports[room],
+                [socket.id]: {
+                    producerTransport: await createWebRtcTransport(room, socket.id, callback)
+                }
+            }
         } else {
-            consumerTransport = await createWebRtcTransport(room, callback)
+            transports[room] = {
+                ...transports[room],
+                [socket.id]: {
+                    consumerTransport: await createWebRtcTransport(room, socket.id, callback)
+                }
+            }
         }
+
+        console.log('transports', transports)
+    })
+
+    socket.on('transport-connect', async ({ dtlsParameters, isCreator, room }) => {
+        console.log('dtlsParameters recieved from socket', socket.id)
+        if(isCreator)
+            await transports[room][socket.id]['producerTransport'].connect({ dtlsParameters })
+        else
+            await transports[room][socket.id]['consumerTransport'].connect({ dtlsParameters })
+    })
+
+    socket.on('transport-produce', async ({ room, kind, rtpParameters, appData }, callback) => {
+        let socketProducerTransport = transports[room][socket.id]['producerTransport']
+
+        transports[room][socket.id]['producer'] = await socketProducerTransport.produce({
+            kind, rtpParameters
+        })
+        let socketProducer = transports[room][socket.id]['producer']
+
+        socketProducer.on('transportclose', () => {
+            console.log('Transport for', socket.id, 'has closed')
+            socketProducer.close()
+        })
+
+        console.log('transports', transports)
+
+        callback({ id: socketProducer.id })
+    })
+
+    socket.on('consume', async({ rtpCapabilities, room }, callback) => {
+        try {
+            let producerSocketId = Object.keys(transports[room]).filter(item => item !== socket.id)
+            let producer = transports[room][producerSocketId]['producer']
+            
+            if(routers[room].canConsume({
+                producerId: producer.id,
+                rtpCapabilities
+            })) {
+                transports[room][socket.id]['consumer'] = await transports[room][socket.id]['consumerTransport'].consume({
+                    producerId: producer.id,
+                    rtpCapabilities,
+                    paused: true
+                })
+
+                let tempConsumer = transports[room][socket.id]['consumer']
+
+                tempConsumer.on('transportclose', () => {
+                    console.log('Transport for', socket.id, 'has closed')
+                })
+
+                tempConsumer.on('producerclose', () => {
+                    console.log('Producer of', socket.id, 'consumer has closed')
+                })
+
+                const params = {
+                    id: tempConsumer.id,
+                    producerId: producer.id,
+                    kind: tempConsumer.kind,
+                    rtopParameters: tempConsumer.rtpParameters
+                }
+
+                console.log('Consumer', tempConsumer.id, 'created')
+                console.log('transports', transports)
+
+                callback({
+                    params: {
+                        id: tempConsumer.id,
+                        producerId: producer.id,
+                        kind: tempConsumer.kind,
+                        rtpParameters: tempConsumer.rtpParameters
+                    }
+                })
+            }
+
+        } catch(error) {
+            console.log('Error on consume', error.message)
+            callback({
+                params: {
+                    error
+                }
+            })
+        }
+    })
+
+    socket.on('consumer-resume', async({ room }) => {
+        transports[room][socket.id]['consumer'].resume()
     })
 
     socket.on('disconnect', () => {
@@ -86,29 +200,29 @@ io.on('connection', async(socket) => {
     })
 })
 
-const createWebRtcTransport = async(room, callback) => {
+const createWebRtcTransport = async (room, currSocketId, callback) => {
     try {
         const webRtcTransportOptions = {
             listenIps: [
                 {
-                  ip: '0.0.0.0', // replace with server's public IP address
-                  announcedIp: '127.0.0.1',
+                    ip: '0.0.0.0', // replace with server's public IP address
+                    announcedIp: '127.0.0.1',
                 }
             ],
             enableUdp: true,
             enableTcp: true,
             preferUdp: true,
         }
-    
+
         let transport = await routers[room].createWebRtcTransport(webRtcTransportOptions)
         console.log('Transport', transport.id, 'created')
-    
+
         transport.on('dtlsstatechange', dtlsState => {
-            if(dtlsState === 'closed') {
+            if (dtlsState === 'closed') {
                 transport.close()
             }
         })
-    
+
         transport.on('close', () => {
             console.log('Transport', transport.id, 'closed')
         })
@@ -118,12 +232,13 @@ const createWebRtcTransport = async(room, callback) => {
                 id: transport.id,
                 iceParameters: transport.iceParameters,
                 iceCandidates: transport.iceCandidates,
-                dtlsParameters: transport.dtlsParameters
+                dtlsParameters: transport.dtlsParameters,
+                // otherSocketIds: Object.keys(transports[room]).filter(id => id !== currSocketId)
             }
         })
 
         return transport
-        
+
     } catch (error) {
         console.log(error)
 
